69508a8533fd44f47467847433c10a509374324b
Hieu02072004 <nguyenminhhieust10@gmail.com>
2024-07-24 04:35:35 +0700

    buoi1

---
+++ be/src/app.js
@@ -0,0 +1,32 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+
+import { connectDB } from "./config/db";
+import authRouter from "./routers/auth";
+import productRouter from "./routers/product";
+import categoryRouter from "./routers/category";
+import cartRouter from "./routers/cart";
+import orderRouter from "./routers/order";
+const app = express();
+dotenv.config();
+// middleware
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+
+// connect db
+connectDB("mongodb://127.0.0.1:27017/xuongreact");
+
+// routers
+app.use("/api", authRouter);
+app.use("/api", productRouter);
+app.use("/api", categoryRouter);
+app.use("/api", cartRouter);
+app.use("/api", orderRouter);
+
+export const viteNodeApp = app;
